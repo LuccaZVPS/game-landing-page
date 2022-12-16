19f83982daefd87380ade4ec96559f3621fa25c2
Lucca <luccazvps@gmail.com>
2022-12-16 13:07:13 -0300

    feat: add wrapper component

---
+++ src/Components/Wrapper/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  font-family: Gotham;
+`;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { Wrapper } from "./Components/Wrapper";
+import { GlobalStyle } from "./Styles/GlobalStyles";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <></>
+  <>
+    <Wrapper />
+    <GlobalStyle />
+  </>
 );
