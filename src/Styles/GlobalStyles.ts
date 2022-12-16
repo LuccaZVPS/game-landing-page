c663936c8626a41f9ef98e2c33df192165cf3ea4
Lucca <luccazvps@gmail.com>
2022-12-16 12:58:11 -0300

    feat: add globalStyles

---
+++ src/Styles/GlobalStyles.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    @font-face { font-family: Gotham; src: url('Gotham-Font/GothamMedium.otf'); } 
+     @font-face { font-family: Gotham; font-weight: bold; src: url('/Gotham-Font/Gotham-Bold.otf');}
+     @font-face { font-family: Gotham; font-weight: 600; src: url('/Gotham-Font/GothamBook.otf');;}
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    scroll-behavior: smooth;
+}
+:root{
+    --BASE: #1C1C28;
+    --BACKGROUND:#DEE1FF;
+    --NEUTRAL:#FFFFFF;
+    --GREY:#777B9E;
+    --PRIMARY:#596AFE;
+    --SECONDARY:#080357;
+    --ACCENT:#F68E5F;
+}
+
+
+
+
+`;
